4358bec42c4d88688f080a703af64799efd139f3
Oillim <nguyenminh29803@gmail.com>
2022-11-07 10:14:33 +0700

    demo-commit

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import { Container,Heading, ListItem,List } from "@chakra-ui/react"
+
+
+
+const Abouts = () =>{
+    return (
+        <Container>
+        <Heading>
+            About me
+        </Heading>
+        Hello readers, my full name is Nguyen Minh Vu, I'm now 19 years old, studying at University of Science, VNU-HCM.  As a second-year student, I had suffered from lots of things such as stresses, peer-pressure environment,... 
+        Luckily, I dont have to retake any course :3 though my grade is not too impressive. However, my goal is not good grades at class but to gain more knowledge from my teachers, my smurf friends @@,...
+    
+        
+        <Heading>Story</Heading>
+        
+        </Container>
+    )
+}
+
+export default Abouts
